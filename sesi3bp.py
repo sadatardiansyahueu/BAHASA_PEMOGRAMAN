48c474cdaf4f85ac1fa95aa0ec6c13799522f10e
sadatardiansyahueu <sadatardiansyah04@student.esaunggul.ac.id>
2022-10-03 22:16:49 +0700

    hello

--- sesi3bp.py
+++ sesi3bp.py
@@ -1,31 +1,36 @@
+print("nama : Sadat Ardiansyah")
 print("NIM : 20210801180")
-print("NAMA : Sadat Ardiansyah")
-
-#Penjumlahan
-print("buah")
-print(13 + 2)
-kurma = 13
-kedondong = 2
-buah = kurma + kedondong 
-
-#Pengurangan
-hutang = 25000
-bayar = 5000
+print("Tugas Bahasa Pemogroman")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("penjumlahan")
+print("15 + 1 =")
+karet = 15
+lolipop = 1
+permen = karet + lolipop 
+print(karet + lolipop)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("pengurangan")
+print("hutang = 5000 dan bayar = 1000")
+print("jadi 5000-1000 = ")
+hutang = 5000
+bayar = 1000
 sisaHutang = hutang - bayar
-print("Sisa hutang Anda adalah ", sisaHutang)
-
-#Perkalian
+print("jadi hutang Anda adalah ", sisaHutang)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("perkalian")
 panjang = 20
 lebar = 5
-luas = ("20 * 5")
-print("luas")
-
-#Pembagian
-masker = 50
-remaja = 5
-maskerPerRemaja = masker / remaja
-print("Setiap remaja akan mendapatkan bagian masker sebanyak ", maskerPerRemaja)
-
+total = panjang * lebar
+print("perkalian 20*5")
+print(total)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("pembagian")
+print("pembagian 50 permen terhadap 5 orang anak kecil")
+coklat = 50
+anak = 5
+coklat_total = coklat / anak 
+print("Setiap anak akan mendapatkan coklat sebanyak ", coklat_total)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print ("Penjumlahan matriks ordo 2x2")
 
 matriks1 = [
@@ -35,8 +40,8 @@ matriks1 = [
 ]
 
 matriks2 = [
-    [30, 40] ,
-    [45, 55] ,
+    [10,12] ,
+    [13,14] ,
 
 ]
 for x in range(0, len(matriks1)) :
@@ -44,3 +49,5 @@ for x in range(0, len(matriks1)) :
         print (matriks1 [x] [y] + matriks2 [x] [y], end='')
     print ('')
     
+print("================")
+print("Terima Kasih")
