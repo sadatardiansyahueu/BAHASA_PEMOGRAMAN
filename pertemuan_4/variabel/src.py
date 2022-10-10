decc51e6e36bffa919a9da5e67821e7b438da9f3
sadatardiansyahueu <sadatardiansyah04@student.esaunggul.ac.id>
2022-10-10 12:24:48 +0700

    typedata

---
+++ pertemuan_4/variabel/src.py
@@ -0,0 +1,27 @@
+#variabel
+a = "sadat ardiansyah"
+def func():
+	a = "any"
+	print :("Selamat " + a)
+func()
+print (a)
+
+#definisi
+def tambah():
+	a = 20
+	b = 25
+	c = a + b
+	print (c)
+
+tambah ()
+
+#definisi parameter
+def data (nama,nim):
+	print(f"nama saya {nama} dan {nim}")
+data("sadat","20210801180")
+
+#contoh
+def total(sisi):
+	return sisi*sisi
+def segitiga(alas,tinggi):
+	return 0.5*alas*tinggi
